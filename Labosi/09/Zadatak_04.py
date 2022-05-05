fc0edca1696fb430c46d8a71824584cdbd3a4ce9
junkie385 <35150776+junkie385@users.noreply.github.com>
2022-05-05 10:28:53 +0200

    commit

---
+++ Labosi/09/Zadatak_04.py
@@ -0,0 +1,5 @@
+# Vježba 4:
+# U ovoj vježbi je cilj vježbati uporabu cijelih i decimalnih brojeva s tekstualnim datotekama.
+# Budući metoda write prima kao argument znakovni niz, demonstrirati kako se u datoteku 
+# insertiraju cjelobrojne i decimalne vrijednosti. Potrebno je demonstrirati uporabu metode str
+# te pomoću operatora formatiranja (malo istražite ovu opciju).
